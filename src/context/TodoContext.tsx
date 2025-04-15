002bf9f6fc349eb63b74b17fd8fbc7e7c5d58c78
Rudolph Pereira <rudolph.pereira93@gmail.com>
2025-04-16 00:20:25 +0530

    bug fix

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -37,7 +37,7 @@ interface Tags {}
 
 export function TodoProvider({ children }: Props) {
   const storedTodos = JSON.parse(localStorage.getItem("todos") ?? "[]");
-  const [todos, setTodos] = useState<Card[]>(storedTodos.reverse());
+  const [todos, setTodos] = useState<Card[]>(storedTodos);
   const [sortValue, setSortValue] = useState<string>("Default");
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const [searchValue, setSearchValue] = useState<string>("");
